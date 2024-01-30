5c09a32aa44a8e491c70a9a1e94670b16ce809a8
Sumit Nagpure <78203156+sumitnagpure@users.noreply.github.com>
2024-01-30 13:08:05 +0530

    just few changes

--- account/admin.py
+++ account/admin.py
@@ -4,17 +4,18 @@ from .models import *
 
 # Register your models here.
 @admin.register(account)
-class Account(admin.ModelAdmin):
-    list_display = ("first_name", "last_name", "email", "date_joined", "phone_number")
+class account(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "email", "date_joined", "phone_number",)
+    
     @admin.display(ordering="first_name",description="First name")
     def first_name(self,obj):
-        return obj.first_name
+        return obj.user.first_name
     @admin.display(ordering="last_name",description="Last name")
     def last_name(self,obj):
-        return obj.last_name
+        return obj.user.last_name
     @admin.display(ordering="email", description="Email address")
     def email(self,obj):
-        return obj.email
+        return obj.user.email
     @admin.display(ordering="date_joined", description="Date Joined")
     def date_joined(self,obj):
-        return obj.date_joined
+        return obj.user.date_joined

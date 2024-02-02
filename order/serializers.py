632993d6eea182fafea6ef549c4a5ea55fd16123
Sumit Nagpure <78203156+sumitnagpure@users.noreply.github.com>
2024-02-02 15:01:57 +0530

    few order app changes

---
+++ order/serializers.py
@@ -0,0 +1,10 @@
+# review/serialzers.py
+
+from rest_framework import serializers
+from .models import order
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = order
+        fields = ('')
--- order/views.py
+++ order/views.py
@@ -1,3 +1,18 @@
+# order/views.py
 from django.shortcuts import render
+from .models import *
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import OrderSerializer
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+
+class OrderView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        data = order.objects.all()
+        serializer = OrderSerializer(data, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)

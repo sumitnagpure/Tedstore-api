f83ed0566639f17f6b05e66dec73a2573c47f5f3
Sumit Nagpure <78203156+sumitnagpure@users.noreply.github.com>
2024-02-07 17:22:46 +0530

    i

--- product/models.py
+++ product/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class Tag(models.Model):
-    tag_id = models.AutoField(primary_key=True)
+    tag_id =models.AutoField(primary_key=True)
     name = models.CharField(max_length=30, unique=True)
 
     class Meta:
@@ -26,6 +26,7 @@ class Details(models.Model):
 class Product(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
+    image = models.ImageField()
     base_price = models.IntegerField()
     discounted_price = models.IntegerField()
     in_stock = models.BooleanField()
@@ -33,14 +34,13 @@ class Product(models.Model):
     details = models.ForeignKey(
         Details, to_field="name", related_name="Details", on_delete=models.CASCADE
     )
-
+    
     class Meta:
         verbose_name_plural = "Products"
 
     def __str__(self):
         return str(self.title)
 
-
 class ProductImages(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to="media/")
+    image=models.ImageField(upload_to='media/')
